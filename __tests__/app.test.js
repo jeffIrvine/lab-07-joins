55cdf57b5de46cb3245b2b7f0b390acede5a1935
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-12-09 14:26:37 -0800

    working post test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,32 @@
 const request = require('supertest');
+const fs = require('fs');
 const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
 
 describe('', () => {
-  it('post', async() => {
-    const res = await request(app).get('/');
 
-    expect(res.body).toEqual({ hello: 'world' });
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('post', async() => {
+    const res = await request(app)
+      .post('/api/v1/cars')
+      .send({
+        make: 'Lotus',
+        model: 'Exige',
+        color: 'white'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      make: 'Lotus',
+      model: 'Exige',
+      color: 'white'
+    });
   });
 });
